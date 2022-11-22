b1e0a69d75e8beef59b767cd5f1f1df464a80411
Vikas Movva <vikas.s.movva@gmail.com>
2022-11-22 10:42:52 -0500

    change Memory trait to public and finish run_callback function

--- src/cpu.rs
+++ src/cpu.rs
@@ -29,7 +29,7 @@ bitflags! {
 
 
 //Memory Trait
-trait Memory{
+pub trait Memory{
     fn m_read(&self, addr: u16) -> u8;
     
     fn m_write(&mut self, addr: u16, data: u8);
@@ -166,24 +166,214 @@ impl CPU {
         // reset registers
         self.register_a = 0;
         self.register_x = 0;
+        self.register_y = 0;
+        self.stack_pointer = STACK_RESET;
         self.status_register = CpuFlags::from_bits_truncate(0b100100);
         // get start address from 0xFFFC
         self.program_counter = self.m_read_u16(0xFFFC);
     }
     
     pub fn load(&mut self, program: Vec<u8>) {
-        self.memory[0x8000..(0x8000 + program.len())].clone_from_slice(&program[..]);
-        self.m_write_u16(0xFFFC, 0x8000);
+        self.memory[0x6000..(0x6000 + program.len())].clone_from_slice(&program[..]);
+        self.m_write_u16(0xFFFC, 0x6000);
     }
 
     pub fn run(&mut self) {
-        loop {
-            let opcode = self.m_read(self.program_counter);
+        self.run_callback(|_| {});
+    }
+
+    pub fn run_callback<F>(&mut self, mut callback: F)
+    where
+        F: FnMut(&mut CPU),
+    {
+        let ref opcodes: HashMap<u8, &'static opcodes::Opcode> = *opcodes::OPCODES_MAP;
+
+        loop{
+            let code = self.m_read(self.program_counter);
             self.program_counter += 1;
-            match opcode {
-                0x00 => break,
-                _ => panic!("Unknown opcode: {:02X}", opcode),
+            let pc_state = self.program_counter;
+            let opcode = opcodes.get(&code).unwrap();
+
+            match code {
+                0x69 | 0x65 | 0x75 | 0x6D | 0x7D | 0x79 | 0x61 | 0x71 => {
+                    self.adc(&opcode.mode);
+                }
+                0x29 | 0x25 | 0x35 | 0x2D | 0x3D | 0x39 | 0x21 | 0x31 => {
+                    self.and(&opcode.mode);
+                }
+                0x0A | 0x06 | 0x16 | 0x0E | 0x1E => {
+                    self.asl(&opcode.mode);
+                }
+                0x90 => {
+                    self.bcc();
+                }
+                0xB0 => {
+                    self.bcs();
+                }
+                0xF0 => {
+                    self.beq();
+                }
+                0x24 | 0x2C => {
+                    self.bit(&opcode.mode);
+                }
+                0x30 => {
+                    self.bmi();
+                }
+                0xD0 => {
+                    self.bne();
+                }
+                0x10 => {
+                    self.bpl();
+                }
+                0x00 => {
+                    return;
+                }
+                0x50 => {
+                    self.bvc();
+                }
+                0x70 => {
+                    self.bvs();
+                }
+                0x18 => {
+                    self.clc();
+                }
+                0xD8 => {
+                    self.cld();
+                }
+                0x58 => {
+                    self.cli();
+                }
+                0xB8 => {
+                    self.clv();
+                }
+                0xC9 | 0xC5 | 0xD5 | 0xCD | 0xDD | 0xD9 | 0xC1 | 0xD1 => {
+                    self.cmp(&opcode.mode);
+                }
+                0xE0 | 0xE4 | 0xEC => {
+                    self.cpx(&opcode.mode);
+                }
+                0xC0 | 0xC4 | 0xCC => {
+                    self.cpy(&opcode.mode);
+                }
+                0xC6 | 0xD6 | 0xCE | 0xDE => {
+                    self.dec(&opcode.mode);
+                }
+                0xCA => {
+                    self.dex();
+                }
+                0x88 => {
+                    self.dey();
+                }
+                0x49 | 0x45 | 0x55 | 0x4D | 0x5D | 0x59 | 0x41 | 0x51 => {
+                    self.eor(&opcode.mode);
+                }
+                0xE6 | 0xF6 | 0xEE | 0xFE => {
+                    self.inc(&opcode.mode);
+                }
+                0xE8 => {
+                    self.inx();
+                }
+                0xC8 => {
+                    self.iny();
+                }
+                0x4C | 0x6C => {
+                    self.jmp(&opcode.mode);
+                }
+                0x20 => {
+                    self.jsr();
+                }
+                0xA9 | 0xA5 | 0xB5 | 0xAD | 0xBD | 0xB9 | 0xA1 | 0xB1 => {
+                    self.lda(&opcode.mode);
+                }
+                0xA2 | 0xA6 | 0xB6 | 0xAE | 0xBE => {
+                    self.ldx(&opcode.mode);
+                }
+                0xA0 | 0xA4 | 0xB4 | 0xAC | 0xBC => {
+                    self.ldy(&opcode.mode);
+                }
+                0x4A | 0x46 | 0x56 | 0x4E | 0x5E => {
+                    self.lsr(&opcode.mode);
+                }
+                0xEA => {
+                    self.nop();
+                }
+                0x09 | 0x05 | 0x15 | 0x0D | 0x1D | 0x19 | 0x01 | 0x11 => {
+                    self.ora(&opcode.mode);
+                }
+                0x48 => {
+                    self.pha();
+                }
+                0x08 => {
+                    self.php();
+                }
+                0x68 => {
+                    self.pla();
+                }
+                0x28 => {
+                    self.plp();
+                }
+                0x2A | 0x26 | 0x36 | 0x2E | 0x3E => {
+                    self.rol(&opcode.mode);
+                }
+                0x6A | 0x66 | 0x76 | 0x6E | 0x7E => {
+                    self.ror(&opcode.mode);
+                }
+                0x40 => {
+                    self.rti();
+                }
+                0x60 => {
+                    self.rts();
+                }
+                0xE9 | 0xE5 | 0xF5 | 0xED | 0xFD | 0xF9 | 0xE1 | 0xF1 => {
+                    self.sbc(&opcode.mode);
+                }
+                0x38 => {
+                    self.sec();
+                }
+                0xF8 => {
+                    self.sed();
+                }
+                0x78 => {
+                    self.sei();
+                }
+                0x85 | 0x95 | 0x8D | 0x9D | 0x99 | 0x81 | 0x91 => {
+                    self.sta(&opcode.mode);
+                }
+                0x86 | 0x96 | 0x8E => {
+                    self.stx(&opcode.mode);
+                }
+                0x84 | 0x94 | 0x8C => {
+                    self.sty(&opcode.mode);
+                }
+                0xAA => {
+                    self.tax();
+                }
+                0xA8 => {
+                    self.tay();
+                }
+                0xBA => {
+                    self.tsx();
+                }
+                0x8A => {
+                    self.txa();
+                }
+                0x9A => {
+                    self.txs();
+                }
+                0x98 => {
+                    self.tya();
+                }
+                _ => {
+                    println!("Unknown opcode: {:X}", code);
+                    return;
+                }
             }
+
+            if pc_state == self.program_counter{
+                self.program_counter += (opcode.bytes - 1) as u16;
+            }
+
+            callback(self);
         }
     }
 
@@ -850,7 +1040,7 @@ impl CPU {
     }
 
     // Unofficial Instructions
-
+    #[allow(dead_code)]
     // ANC - AND with Carry
     fn anc(&mut self, mode: &AddressingMode){
         // get the address of the operand and read the data
